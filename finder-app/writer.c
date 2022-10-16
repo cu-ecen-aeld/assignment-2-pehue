e435c983c81c79311c5615675be50b4e1fc2a997
pedro <peter.huesser@psi.ch>
2022-10-16 20:13:15 +0200

    finished log

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <syslog.h>
 
 int main(int argc, char ** argv) {
+    openlog(NULL, 0, LOG_USER);
+
     if (argc != 3) {
-        fprintf(stderr, "Something is wrong with the parameters\n");
+        //fprintf(stderr, "Something is wrong with the parameters\n");
+    	syslog(LOG_ERR, "Something is wrong with the parameters\n");
         exit(EXIT_FAILURE);
     }
 
     FILE * f = fopen(argv[1], "w");
     fprintf(f, "%s\n", argv[2]);
+    syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
     fclose(f);
 
     return 0;
